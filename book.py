2b6eda8e37ec945a609229dbe214ab1f4f2ea1f8
马可 <78850005+makebl@users.noreply.github.com>
2023-11-22 14:21:44 +0800

    Update book.py

--- book.py
+++ book.py
@@ -5,6 +5,7 @@ import os
 from datetime import datetime, timedelta
 import re
 import urllib3
+import urllib.parse
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
@@ -65,7 +66,7 @@ def get_redirected_url(url):
         print(f"Status Code: {response.status_code}")
         print(f"Response Content: {response.text}")
         return None
-        
+
 def download_json(url, output_dir='3.0'):
     final_url = get_redirected_url(url)
 
@@ -108,6 +109,29 @@ def download_json(url, output_dir='3.0'):
             print(f"Response Content: {response.text}")
     else:
         print(f"Error getting redirected URL for {url}")
+
+def clean_old_files(directory='3.0'):
+    os.makedirs(directory, exist_ok=True)
+
+    for filename in os.listdir(directory):
+        file_path = os.path.join(directory, filename)
+        if filename.endswith('.json') and filename != 'me.json':
+            os.remove(file_path)
+            print(f"Deleted old file: {filename}")
+
+def merge_json_files(input_dir='3.0', output_file='merged.json'):
+    all_data = []
+
+    for filename in os.listdir(input_dir):
+        if filename.endswith('.json'):
+            with open(os.path.join(input_dir, filename), 'r') as f:
+                data = json.load(f)
+                all_data.extend(data)
+
+    with open(output_file, 'w', encoding='utf-8') as f:
+        json.dump(all_data, f, indent=2, ensure_ascii=False)
+
+    print(f"Successfully merged {len(all_data)} book sources to {output_file}")
 
 def merge_shuyuan_files(input_dir='shuyuan', output_file='shuyuan.json'):
     all_data = []
