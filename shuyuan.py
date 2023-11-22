5f36460b2ae20fd198f37f3c5efbec81fdec2d6e
马可 <78850005+makebl@users.noreply.github.com>
2023-11-22 21:09:06 +0800

    Update shuyuan.py

--- shuyuan.py
+++ shuyuan.py
@@ -127,7 +127,7 @@ def clean_old_files(directory=''):
 
 def merge_json_files(input_dir='', output_file='merged.json'):
     # 如果目录不存在，创建它
-    if not os.path.exists(input_dir):
+    if input_dir and not os.path.exists(input_dir):
         os.makedirs(input_dir)
 
     all_data = {}
@@ -137,6 +137,12 @@ def merge_json_files(input_dir='', output_file='merged.json'):
             with open(os.path.join(input_dir, filename)) as f:
                 data = json.load(f)
                 all_data[filename.split('.')[0]] = data
+
+    output_path = os.path.join(input_dir, output_file)
+    with open(output_path, 'w') as f:
+        # Write JSON content with the outermost square brackets
+        f.write(json.dumps(all_data, indent=2, ensure_ascii=False))
+
 
     output_path = os.path.join(input_dir, output_file)
     with open(output_path, 'w') as f:
