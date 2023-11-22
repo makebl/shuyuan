9fda39fe4143f44637adf041bef78e06129d0c0f
马可 <78850005+makebl@users.noreply.github.com>
2023-11-22 14:06:14 +0800

    Update book.py

--- book.py
+++ book.py
@@ -1,71 +1,3 @@
-import requests
-from bs4 import BeautifulSoup
-import json
-import os
-from datetime import datetime, timedelta
-import re
-import urllib3
-
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-
-def parse_and_transform(url):
-    session = requests.Session()
-    response = session.get(url, verify=False, allow_redirects=True)
-    soup = BeautifulSoup(response.text, 'html.parser')
-
-    relevant_links = []
-    today = datetime.today().date()
-
-    for div in soup.find_all('div', class_='layui-col-xs12 layui-col-sm6 layui-col-md4'):
-        link = div.find('a', href=True)
-        date_element = div.find('p', class_='m-right')
-
-        if link and date_element:
-            href = link['href']
-            link_date_str = date_element.text.strip()
-
-            match = re.search(r'(\d+)(天前|小时前|分钟前)', link_date_str)
-            if match:
-                value, unit = match.group(1, 2)
-                if unit == '分钟前':
-                    # For minutes, consider them as 1 day
-                    days_ago = 1
-                elif unit == '小时前':
-                    # For hours, consider them as 1 day
-                    days_ago = 1
-                else:
-                    days_ago = int(value)
-
-                link_date = today - timedelta(days=days_ago)
-
-                print(f"Link: {href}, Date String: {link_date_str}, Calculated Date: {link_date}")
-
-                # Check if the link is within the specified time range
-                if 1 <= days_ago <= 2:  # Include links from 1 to 3 days ago
-                    json_url = f'https://www.yckceo.com{href.replace("content", "json")}'
-                    relevant_links.append((json_url, link_date))
-
-    return relevant_links
-
-def get_redirected_url(url):
-    session = requests.Session()
-    response = session.get(url, verify=False, allow_redirects=False)
-
-    if response.status_code in (301, 302, 303, 307, 308):
-        final_url = response.headers.get('location')
-        if not final_url.startswith('http'):
-            # If the final URL is relative, make it absolute
-            final_url = urllib.parse.urljoin(url, final_url)
-        return final_url
-    elif response.status_code == 200:
-        # If there was no redirection, return the original URL
-        return url
-    else:
-        print(f"Error getting redirected URL for {url}")
-        print(f"Status Code: {response.status_code}")
-        print(f"Response Content: {response.text}")
-        return None
-
 def download_json(url, output_dir='3.0'):
     final_url = get_redirected_url(url)
 
@@ -92,6 +24,7 @@ def download_json(url, output_dir='3.0'):
                 if link_date is None:
                     link_date = datetime.today().date()
 
+                # Ensure that the file is saved in the specified output directory without subdirectories
                 output_path = os.path.join(output_dir, f'{id}.json')
 
                 with open(output_path, 'w', encoding='utf-8') as f:
@@ -115,7 +48,7 @@ def clean_old_files(directory='3.0'):
             os.remove(file_path)
             print(f"Deleted old file: {filename}")
 
-def merge_json_files(input_dir, output_file):
+def merge_json_files(input_dir='3.0', output_file='merged.json'):
     all_data = []
 
     for filename in os.listdir(input_dir):
@@ -132,18 +65,11 @@ def merge_json_files(input_dir, output_file):
 def main():
     original_url = 'https://www.yckceo.com/yuedu/shuyuan/index.html'
     transformed_urls = parse_and_transform(original_url)
-    
-    # Clean old files before downloading new ones
-    clean_old_files()
-
+    clean_old_files()  # Clean old files before downloading new ones
     for url, _ in transformed_urls:
-        # Decide the output directory based on the URL
-        output_dir = '3.0' if '3.0' in url else 'shuyuan'
-        download_json(url, output_dir)
+        download_json(url)
 
-    # Merge downloaded JSON files
-    merge_json_files('3.0', 'merged.json')
-    merge_json_files('shuyuan', 'shuyuan.json')
+    merge_json_files()  # Merge downloaded JSON files
 
 if __name__ == "__main__":
     main()
