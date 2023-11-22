6349a119b80c4d36661e72d037431372668b6014
马可 <78850005+makebl@users.noreply.github.com>
2023-11-22 12:41:03 +0800

    Update book.py

--- book.py
+++ book.py
@@ -10,6 +10,7 @@ import re
 url = 'https://www.yckceo.com/yuedu/shuyuan/index.html'
 
 def parse_page():
+    session = requests.Session()
     response = session.get(url, verify=False, allow_redirects=True)
     soup = BeautifulSoup(response.text, 'html.parser')
 
